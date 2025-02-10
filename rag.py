2dd65d09bb1e1153531ba85ad1bba4f9bfcc855e
ataing <ataing1883@gmail.com>
2025-02-10 16:37:12 -0500

    turn rag retrieval into a tool

--- rag.py
+++ rag.py
@@ -2,8 +2,12 @@ from langchain import hub
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langgraph.graph import START, StateGraph
+from langgraph.graph import START, StateGraph, MessagesState
 from typing_extensions import List, TypedDict
+from langchain_core.tools import tool
+from langchain_core.messages import SystemMessage
+
+from langgraph.prebuilt import ToolNode
 
 from loader import ResearchLoader
 
@@ -35,8 +39,16 @@ class State(TypedDict):
     answer: str
 
 
-# Define application steps
+@tool(response_format="content")
 def retrieve(state: State):
+    """Retrieve relevant documents from the vector store based on the input question.
+    
+    Args:
+        state: The current state containing the question to search for
+        
+    Returns:
+        dict: A dictionary containing the retrieved documents in the 'context' key
+    """
     retrieved_docs = vector_store.similarity_search(state["question"])
     print("\nRetrieved Documents:")
     for i, doc in enumerate(retrieved_docs):
@@ -46,17 +58,89 @@ def retrieve(state: State):
     return {"context": retrieved_docs}
 
 
-def generate(state: State):
-    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
-    messages = prompt.invoke({"question": state["question"], "context": docs_content})
-    response = llm.invoke(messages)
-    return {"answer": response.content}
+# def generate(state: State):
+#     docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+#     messages = prompt.invoke({"question": state["question"], "context": docs_content})
+#     response = llm.invoke(messages)
+#     return {"answer": response.content}
+
+# Step 1: Generate an AIMessage that may include a tool-call to be sent.
+def query_or_respond(state: MessagesState):
+    """Generate tool call for retrieval or respond."""
+    llm_with_tools = llm.bind_tools([retrieve])
+    response = llm_with_tools.invoke(state["messages"])
+    # MessagesState appends messages to state instead of overwriting
+    return {"messages": [response]}
+
+tools = ToolNode([retrieve])
+
+# Step 3: Generate a response using the retrieved content.
+def generate(state: MessagesState):
+    """Generate answer."""
+    # Get generated ToolMessages
+    recent_tool_messages = []
+    for message in reversed(state["messages"]):
+        if message.type == "tool":
+            recent_tool_messages.append(message)
+        else:
+            break
+    tool_messages = recent_tool_messages[::-1]
+
+    # Format into prompt
+    docs_content = "\n\n".join(doc.content for doc in tool_messages)
+    system_message_content = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        f"{docs_content}"
+    )
+    conversation_messages = [
+        message
+        for message in state["messages"]
+        if message.type in ("human", "system")
+        or (message.type == "ai" and not message.tool_calls)
+    ]
+    prompt = [SystemMessage(system_message_content)] + conversation_messages
+
+    # Run
+    response = llm.invoke(prompt)
+    return {"messages": [response]}
 
 
-# Compile application and test
-graph_builder = StateGraph(State).add_sequence([retrieve, generate])
-graph_builder.add_edge(START, "retrieve")
+# # Compile application and test
+# graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+# graph_builder.add_edge(START, "retrieve")
+# graph = graph_builder.compile()
+
+# response = graph.invoke({"question": "What are the main research papers that discovered neural nets?"})
+# print(response["answer"])
+
+from langgraph.graph import END
+from langgraph.prebuilt import ToolNode, tools_condition
+
+graph_builder = StateGraph(MessagesState)
+graph_builder.add_node(query_or_respond)
+graph_builder.add_node(tools)
+graph_builder.add_node(generate)
+
+graph_builder.set_entry_point("query_or_respond")
+graph_builder.add_conditional_edges(
+    "query_or_respond",
+    tools_condition,
+    {END: END, "tools": "tools"},
+)
+graph_builder.add_edge("tools", "generate")
+graph_builder.add_edge("generate", END)
+
 graph = graph_builder.compile()
 
-response = graph.invoke({"question": "What are the main research papers that discovered neural nets?"})
-print(response["answer"])
+input_message = "what is a neural net"
+
+for step in graph.stream(
+    {"messages": [{"role": "user", "content": input_message}]},
+    stream_mode="values",
+):
+    step["messages"][-1].pretty_print()
